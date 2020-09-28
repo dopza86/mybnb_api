d047a94988f5b5c20d419f89d26b0609fb59121a
dopza86 <dopza86@gmail.com>
2020-09-29 00:44:45 +0900

    viewsets

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,7 +1,14 @@
-
+from rest_framework.routers import DefaultRouter
 from django.urls import path
-from . import views
+from . import views, viewsets
 
 app_name = "rooms"
 
-urlpatterns = [path("list/", views.ListRoomsView.as_view())]
+router = DefaultRouter()
+router.register("rooms", viewsets.RoomViewset, basename="room")
+
+
+urlpatterns = router.urls
+
+# urlpatterns = [path("list/", views.ListRoomsView.as_view()),
+#                path("<int:pk>/", views.SeeRoomView.as_view()), ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,11 +1,21 @@
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Room
-from .serializers import RoomSerializer
+from .serializers import RoomSerializer, BigRoomSerializer
 
 
-class ListRoomsView(APIView):
-    def get(self, request):
-        rooms = Room.objects.all()
-        selializer = RoomSerializer(rooms, many=True)
-        return Response(data=selializer.data)
+class ListRoomsView(ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+# class ListRoomsView(APIView):
+#     def get(self, request):
+#         rooms = Room.objects.all()
+#         serializer = RoomSerializer(rooms, many=True)
+#         return Response(serializer.data)
+
+
+class SeeRoomView(RetrieveAPIView):
+    queryset = Room.objects.all()
+    serializer_class = BigRoomSerializer
