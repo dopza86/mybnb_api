0bcc1ff8922fcd0dec1db24b33d19ad73baf711a
dopza86 <dopza86@gmail.com>
2020-09-30 00:05:29 +0900

    create user

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,14 +1,8 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path
-from . import views, viewsets
+from . import views
+
 
 app_name = "rooms"
-
-router = DefaultRouter()
-router.register("rooms", viewsets.RoomViewset, basename="room")
-
-
-urlpatterns = router.urls
-
-# urlpatterns = [path("list/", views.ListRoomsView.as_view()),
-#                path("<int:pk>/", views.SeeRoomView.as_view()), ]
+urlpatterns = [path("", views.rooms_view),
+               path("<int:pk>/", views.SeeRoomView.as_view()), ]
