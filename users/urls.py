7883c7e3e8a421d7ea8545c903cdbf557287ba1b
dopza86 <dopza86@gmail.com>
2020-10-03 20:29:47 +0900

    jwt token login

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,5 @@ app_name = "users"
 urlpatterns = [path("", views.UsersView.as_view()),
                path("me/", views.MeView.as_view()),
                path("me/favs/", views.FavsView.as_view()),
-               path("<int:pk>", views.user_detail)]
+               path("<int:pk>", views.user_detail),
+               path("token/", views.login), ]
--- users/views.py
+++ users/views.py
@@ -1,12 +1,13 @@
+import jwt
+from django.conf import settings
+from django.contrib.auth import authenticate
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework import status
-
 from rooms.serializers import RoomSerializer
 from rooms.models import Room
-
 from .serializers import UserSerializer
 from .models import User
 
@@ -75,3 +76,19 @@ def user_detail(request, pk):
         return Response(serializer)
     except User.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(["POST"])
+def login(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+    if not username or not password:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    user = authenticate(username=username, password=password)
+
+    if user is not None:
+        encoded_jwt = jwt.encode(
+            {'pk': user.pk}, settings.SECRET_KEY, algorithm='HS256')
+        return Response(data={"token": encoded_jwt})
+    else:
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
